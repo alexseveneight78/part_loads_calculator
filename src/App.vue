7335f0d2579b7c328b662c5c000466fb26b97f9f
Alexandr Abramtchik <alexseveneight@gmail.com>
2019-06-07 08:31:14 +0300

    git command - git add src/

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>Hello there!</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  margin: 50px auto;
+}
+h3 {
+  text-align: center;
+}
+
+</style>
